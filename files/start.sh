feed9e26a03e75dbbd6764b2c75b0ba803fd7c0f
rahul <rahul.pundir1@delhivery.com>
2018-06-29 10:53:22 +0000

    .

---
+++ files/start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+/usr/local/bin/prometheus \
+    --config.file /etc/prometheus/prometheus.yml \
+    --storage.tsdb.path /var/lib/prometheus/ \
+    --web.console.templates=/etc/prometheus/consoles \
+    --web.console.libraries=/etc/prometheus/console_libraries
